b7a9f9056c27a9f19589170ae223f1629f14cf27
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2023-03-16 23:05:28 +1100

    Add instructions for flashing

--- README.md
+++ README.md
@@ -9,6 +9,16 @@ make microdox build    # build microdox.uf2
 
 ![](./reference.png)
 
+## Flashing
+
+Copy the `.uf2` file to the keyboard in bootloader mode. Alternatively,
+
+```bash
+./flash.sh --label TECHNIKABLE technikable.uf2
+./flash.sh --label NICENANO microdox_left.uf2
+# only works on Linux and MacOS
+```
+
 ## Acknowledgements
 
 - [petejohanson/technikable-zmk-config](https://github.com/petejohanson/technikable-zmk-config) &mdash; board definitions for the Technikable were taken from here
--- flash.sh
+++ flash.sh
@@ -5,81 +5,85 @@
 #   flash.sh --label NICENANO microdox_left.uf2
 #
 main() {
-  local label='NICENANO' file=''
+	local label='NICENANO' file=''
 
-  while true; do
-    case "${1:-}" in
-      --label) label="$2"; shift 2 ;;
-      *) break ;;
-    esac
-  done
+	while true; do
+		case "${1:-}" in
+		--label)
+			label="$2"
+			shift 2
+			;;
+		*) break ;;
+		esac
+	done
 
-  file="$1"
+	file="$1"
 
-  if [[ -z "${file}" ]]; then
-    echo 'Usage: flash.sh [--label LABEL] path/to/file.uf2'
-    exit 1
-  fi
+	if [[ -z "${file}" ]]; then
+		echo 'Usage: flash.sh [--label LABEL] path/to/file.uf2'
+		exit 1
+	fi
 
-  if [[ -d /mnt/wsl ]]; then
-    # WSL
-    # flash-by-cp "/mnt/e" "$file"
-    echo '-- Sorry, no WSL support yet :('
-    exit 1
-  elif [[ -d /dev/disk/by-label ]]; then
-    # Linux
-    flash-by-mount "/dev/disk/by-label/$label" "$file"
-  elif [[ -d /Volumes ]]; then
-    # MacOS
-    flash-by-cp "/Volumes/$label" "$file"
-  else
-    echo '-- Sorry, your platform is unsupported'
-    exit 1
-  fi
+	if [[ -d /mnt/wsl ]]; then
+		# WSL
+		# flash-by-cp "/mnt/e" "$file"
+		echo '-- Sorry, no WSL support yet :('
+		exit 1
+	elif [[ -d /dev/disk/by-label ]]; then
+		# Linux
+		flash-by-mount "/dev/disk/by-label/$label" "$file"
+	elif [[ -d /Volumes ]]; then
+		# MacOS
+		flash-by-cp "/Volumes/$label" "$file"
+	else
+		echo '-- Sorry, your platform is unsupported'
+		exit 1
+	fi
 
-  echo '-- Done!'
+	echo '-- Done!'
 }
 
 # flash-by-mount "/dev/disk/by-label/NICENANO" "file.uf2"
 flash-by-mount() {
-  local device="$1"
-  local file="$2"
-  echo ""
-  echo "━━› Flashing $file"
-  echo -n "━━› Waiting for $device"
-  while true; do
-    if [[ -e "$device" ]]; then break; fi
-    sleep 0.2
-    echo -n '.'
-  done
-  echo ' ✓'
-  echo -n '-- Mounting...'
-  mkdir -p /tmp/flashmnt
-  mount "$device" /tmp/flashmnt
-  echo ' ✓'
+	local device="$1"
+	local file="$2"
+	echo ""
+	echo "━━› Waiting for $device"
+	sudo true
+	echo "━━› "
+	while true; do
+		if [[ -e "$device" ]]; then break; fi
+		sleep 0.2
+		echo -n '.'
+	done
+	echo ' ✓'
+	echo -n '-- Mounting...'
+	mkdir -p /tmp/flashmnt
+	sudo mount "$device" /tmp/flashmnt
+	echo ' ✓'
 
-  echo -n "━━› Sending $file..."
-  cp "$file" /tmp/flashmnt
-  sync
-  umount -R /tmp/flashmnt
-  echo ' ✓'
+	echo -n "━━› Sending $file..."
+	cp "$file" /tmp/flashmnt
+	sync
+	sudo umount -R /tmp/flashmnt
+	echo ' ✓'
 }
 
 # flash-by-cp "/Volumes/NICENANO" "file.uf2"
 flash-by-cp() {
-  local dest="$1"
-  local file="$2"
-  echo -n "━━› Waiting for $dest"
-  while true; do
-    if [[ -e "$dest/CURRENT.UF2" ]]; then break; fi
-    sleep 0.2
-    echo -n '.'
-  done
-  echo ' ✓'
+	local dest="$1"
+	local file="$2"
+	echo -n "━━› Waiting for $dest"
+	while true; do
+		if [[ -e "$dest/CURRENT.UF2" ]]; then break; fi
+		sleep 0.2
+		echo -n '.'
+	done
+	echo ' ✓'
 
-  echo -n "━━› Sending $file..."
-  cp "$file" "$dest/"
-  echo ' ✓'
+	echo -n "━━› Sending $file..."
+	cp "$file" "$dest/"
+	echo ' ✓'
 }
 
 main "$@"
